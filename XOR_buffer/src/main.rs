b4ed1e349ec8b2e6258a525de3b1eaec9800771b
Erik Hartig <wishfulthinkin16@gmail.com>
2017-12-08 03:40:32 -0600

    lots of bug fixes to xor

--- XOR_buffer/src/main.rs
+++ XOR_buffer/src/main.rs
@@ -4,50 +4,49 @@ use std::collections::HashMap;
 fn main() {
     println!("{}", hex_decoder("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"));
     assert_eq!(hex_decoder("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"),"746865206b696420646f6e277420706c6179");
-    let mut digrams = xor_decrypter("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+    let digrams = xor_decrypter("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
     println!("{}", digrams);
 }
 
 fn xor_decrypter(str_decrypt: &str) -> String{
-    let mut digrams = digramCounter(str_decrypt);
-    let largest = getLargest(digrams);
-    digrams.remove(largest);
-    let xor_key = hex_decoder(largest, "65");
-    let mut test_hex = "";
+    let mut digrams = digram_counter(str_decrypt);
+    let largest = get_largest(digrams);
+    //digrams.remove(&largest);
+    let xor_key = hex_decoder(&largest, "65");
+    let mut test_hex = String::from("");
     while test_hex.len() < str_decrypt.len() {
-        test_hex.push_str(xor_key);
+        test_hex = test_hex.clone() + &xor_key;
     }
-    let result = hex_decoder(test_hex, str_decrypt);
+    let result = hex_decoder(&test_hex, &str_decrypt);
     return result;
 }
 
-fn getLargest(hex_count: HashMap<String, i32>) -> String {
-    let mut largest_key =  "";
-    let mut largest_value = 0;
+fn get_largest(hex_count: HashMap<String, i32>) -> String {
+    let mut largest_key =  String::from("");
+    let mut largest_value = &i32::from(0);
     for (key, val) in hex_count.iter() {
         if val > largest_value{
-            largest_key = key;
+            largest_key = key.clone();
             largest_value = val;
         }
     }
     return largest_key;
-
 }
-fn digramCounter(hex_string:&str) -> HashMap<String, i32> {
-    let mut hex_vector = hex_string.chars().collect();
+fn digram_counter(hex_string:&str) -> HashMap<String, i32> {
+    let mut hex_vector: Vec<char>= hex_string.chars().collect();
     let mut hex_digram = HashMap::new();
     while hex_vector.len() > 0{
         let hex_1 = hex_vector.pop().unwrap() as char;
         let hex_2 = hex_vector.pop().unwrap() as char;
-        let hexDouble = hex_1.to_string().push_str(hex_2.to_string());
-        if  hex_digram.contains_key(hexDouble){
+        let hexDouble = hex_1.to_string().clone() + &hex_2.to_string();
+        if  hex_digram.contains_key(&hexDouble){
             *hex_digram.entry(hexDouble).or_insert(1) +=1;
         }
         else {
             hex_digram.insert(hexDouble, 1);
         }
     }
-    return hex_digram;
+    return hex_digram.clone();
 }
 
 fn hex_decoder(hex_string_1:&str, hex_string_2:&str) -> String {
@@ -65,7 +64,7 @@ fn hex_decoder(hex_string_1:&str, hex_string_2:&str) -> String {
         let mut hex_2 = hex_vector_2.pop().unwrap().to_digit(16).unwrap() as u8;
         let mut hex_xor = 0;
         for x in 0 .. 4 {
-            hex_xor = hex_xor + 2.pow(x)*xorBit(hex_1 % 2, hex_2 % 2);
+            hex_xor = hex_xor + (2u8).pow(x)*xorBit(&(hex_1 % 2), &(hex_2 % 2));
             hex_1 = (hex_1 - hex_1 % 2)/2;
             hex_2 = (hex_2 - hex_2 % 2)/2;
         }
@@ -76,13 +75,13 @@ fn hex_decoder(hex_string_1:&str, hex_string_2:&str) -> String {
 
 
 fn xorBit(hex_1: &u8, hex_2: &u8) -> u8{
-    if hex_1 == 1 && hex_2 == 1 {
-        return 0;
+    if hex_1.clone() == 1u8 && hex_2.clone() == 1u8 {
+        return 0u8;
     }
-    else if hex_1 == 1 || hex_2 == 1 {
-        return 1;
+    else if hex_1.clone() == 1u8 || hex_2.clone() == 1u8 {
+        return 1u8;
     }
     else {
-        return 0;
+        return 0u8;
     }
 }
