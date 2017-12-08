f70c0d00a6e138c010e25b12d2b511c0513efb29
MilesMcD <s.eizon@yahoo.com>
2017-12-07 23:25:49 -0600

    refactored question 2
    let's do this

--- XOR_buffer/src/main.rs
+++ XOR_buffer/src/main.rs
@@ -12,8 +12,8 @@ fn hexDecoder(hex_string_1:&str, hex_string_2:&str) -> String {
     let mut hex_vector_3 = VecDeque::new();
     let hex_converter: Vec<char> = "0123456789abcdef".chars().collect();
     while hex_vector_1.len() > 0 {
-        let mut hex_1 = hex_vector_1.pop().unwrap().to_digit(16).unwrap() as u8;
-        let mut hex_2 = hex_vector_2.pop().unwrap().to_digit(16).unwrap() as u8;
+        let mut hex_1 = unwrap_vector(hex_vector_1.pop());
+        let mut hex_2 = unwrap_vector(hex_vector_2.pop());
         let mut hex_xor = 0;
         for x in 0 ... 4 {
             hex_xor = hex_xor + 2.pow(x)*xorBit(hex_1 % 2, hex_2 % 2);
@@ -35,3 +35,8 @@ fn xorBit(hex_1: &u8, hex_2: &u8) -> u8{
         return 0;
     }
 }
+fn unwrap_vector(hex: Option<char>) -> u8 {
+	let x: u8 = hex.unwrap().to_digit(16).unwrap() as u8;
+	return x;
+}
+
