88750f46e938e2327a0624b435915386906ebc13
MilesMcD <s.eizon@yahoo.com>
2017-12-08 03:15:09 -0600

    Added hex to ASCII function
    to XOR

--- XOR_buffer/src/main.rs
+++ XOR_buffer/src/main.rs
@@ -3,13 +3,14 @@ use std::collections::VecDeque;
 fn main() {
 println!("{}", hex_decoder("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"));
 assert_eq!(hex_decoder("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965"),"746865206b696420646f6e277420706c6179");
-
+//println!("{}", hex_to_ascii("68656c6c6f"));
+hex_to_ascii(String::from("68656c6c6f"));
 }
 
 fn hex_decoder(hex_string_1:&str, hex_string_2:&str) -> String {
 	if hex_string_1.len() != hex_string_2.len()
 	{
-				panic!("strings are of differing lengths");
+		panic!("strings are of differing lengths");
 	}
 
     let mut hex_vector_1: Vec<char> = hex_string_1.chars().collect();
@@ -44,5 +45,28 @@ fn xor_bit(hex_1: u8, hex_2: u8) -> u8{
 fn unwrap_vector(hex: Option<char>) -> u8 {
 	let x: u8 = hex.unwrap().to_digit(16).unwrap() as u8;
 	return x;
+}
+//hex: Vec<char>
+
+
+fn hex_to_ascii(hex_encoded_str: String){
+	let alphabet: Vec<char> = 
+	" !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz".chars().collect();
+	let mut final_string: String = String::from("");
+	
+	let hex: Vec<_> = hex_encoded_str.chars().collect();
+	
+    let decimal_encoded: Vec<usize> = hex.chunks(2).map(|chunk| {
+        let first_byte = chunk[0].to_digit(16).unwrap() as usize;
+        let second_byte = chunk[1].to_digit(16).unwrap() as usize;
+
+        ((first_byte * 16) + second_byte - 32)//Alphabet starts at position 32 of ASCII chart
+    }).collect();
+	
+	for x in 0..decimal_encoded.len() {
+	final_string.push(alphabet[decimal_encoded[x]]);
+	}
+	println!("{}", final_string);
+	
 }
 
