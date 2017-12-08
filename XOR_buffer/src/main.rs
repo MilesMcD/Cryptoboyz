28895ab5293560976a803f3107ef2d8824f733a0
Erik Hartig <wishfulthinkin16@gmail.com>
2017-12-07 23:22:55 -0600

    worked on xor buffer and finished functionality

--- XOR_buffer/src/main.rs
+++ XOR_buffer/src/main.rs
@@ -1,21 +1,26 @@
+use std::collections::VecDeque;
+
 fn main() {
     println!("Hello, world!");
 
 
 }
 
-fn hexDecoder(hex_string:&str) -> String {
+fn hexDecoder(hex_string_1:&str, hex_string_2:&str) -> String {
     let mut hex_vector_1: Vec<char> = hex_string_1.chars().collect();
-    let mut hex_vector_1: Vec<char> = hex_string_2.chars().collect();
+    let mut hex_vector_2: Vec<char> = hex_string_2.chars().collect();
+    let mut hex_vector_3 = VecDeque::new();
+    let hex_converter: Vec<char> = "0123456789abcdef".chars().collect();
     while hex_vector_1.len() > 0 {
         let mut hex_1 = hex_vector_1.pop().unwrap().to_digit(16).unwrap() as u8;
         let mut hex_2 = hex_vector_2.pop().unwrap().to_digit(16).unwrap() as u8;
         let mut hex_xor = 0;
-        for x in 1 ... 5 {
-            hex_xor = hex_xor + xorBit(hex_1 % 2, hex_2 % 2);
+        for x in 0 ... 4 {
+            hex_xor = hex_xor + 2.pow(x)*xorBit(hex_1 % 2, hex_2 % 2);
             hex_1 = (hex_1 - hex_1 % 2)/2;
             hex_2 = (hex_2 - hex_2 % 2)/2;
         }
+        base64.push_front(hex_converter[hex_xor as usize]);
     }
 }
 
